700a8cebb5bad51f3145480fa24e1d9f1c04392f
Developer-Madhu <madhuatomix@gmail.com>
2024-12-30 18:01:50 +0530

    updated & added api data

--- BrowserUI/src/Components/Stock.jsx
+++ BrowserUI/src/Components/Stock.jsx
@@ -6,6 +6,7 @@ const Stock = () => {
 
   const [data, setData] = useState('');
   const [data2, setData2] = useState('');
+  const [] = useState('')
 
   useEffect(() => {
     const fetchData = async (c) => {
@@ -17,19 +18,33 @@ const Stock = () => {
         }
         const result = await response.json();
         const result_2 = await dailyresponse.json();
-        // console.log(result_2)
+        console.log(result_2)
+
+        const timeSeries = result_2["Time Series (Daily)"]
+
+        const dates = Object.keys(timeSeries).sort((a, b) => new Date(b) - new Date(a));
+
+        // Get the latest date
+        const latestDate = dates[0];
+        const latestData = timeSeries[latestDate];
+
+        console.log("Latest Date:", latestDate);
+        console.log("Latest Data:", latestData);
+
         // console.log(result) 
 
         setData({
           marketRegion: result.markets[8].region,
           marketType: result.markets[8].market_type,
-          marketStatus: result.markets[8].current_status
+          marketStatus: result.markets[8].current_status,
+          marketDate: latestDate,
+          open: latestData["1. open"]
         })
 
         setData2({
           presentTime: result_2["Time Series (Daily)"]
         })
-        console.log(data2.presentTime)
+        console.log(data2)
 
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -41,10 +56,19 @@ const Stock = () => {
 
   return (
     <div className='stockdivs'>
+
       <div className='div1'>
         <h1>{data.marketRegion}</h1>
-        <h3>{data.marketType} - {data.marketStatus}</h3>
+        <div className="txt">
+          <h3>{data.marketType} - {data.marketStatus}</h3>
+          <p>{data.marketDate}</p>
+        </div>
+        <div className="txt">
+          <h3>Open - {data.open}</h3>
+          <h3>Close - {data.close}</h3>
+        </div>
       </div>
+
       <div className='div2'>
 
         <div className="one">
@@ -69,7 +93,18 @@ const Stock = () => {
         </div>
 
       </div>
-      <div className='div3'></div>
+
+      <div className='div1'>
+        <h1>{data.marketRegion}</h1>
+        <div className="txt">
+          <h3>{data.marketType} - {data.marketStatus}</h3>
+          <p>{data.marketDate}</p>
+        </div>
+        <div className="txt">
+          <h3>Open - {data.open}</h3>
+          <h3>Close - {data.close}</h3>
+        </div>
+      </div>
     </div>
   )
 }
